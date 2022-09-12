3a44f60dc28335b4aecb33b8a02b0e955ef60d2e
timbynce <cherar90@gmail.com>
2022-09-12 14:24:51 +0300

    Sign out specs

--- spec/features/answer/create_spec.rb
+++ spec/features/answer/create_spec.rb
@@ -10,12 +10,19 @@ feature 'User can open question page with list of answers', %q{
   given!(:question) { create(:question) }
 
   describe 'Authenticated user' do
-    scenario 'list all answers of question' do
+    scenario 'create answer of question' do
       sign_in(user)
       visit question_path(question)
       fill_in 'Answer', with: 'answer text text'
       click_on 'Send answer'
       expect(page).to  have_content 'Your answer successfully created.'
+    end
+
+    scenario 'create answer of question with errors', :js => true do
+      sign_in(user)
+      visit question_path(question)
+      click_on 'Send answer'
+      expect(page).to have_content "Body can't be blank"
     end
   end
 end
--- spec/features/question/create_spec.rb
+++ spec/features/question/create_spec.rb
@@ -29,9 +29,7 @@ feature 'User can create question', %q{
 
     scenario 'asks a question with errors' do    
       click_on 'Ask'
-
       expect(page).to have_content "Title can't be blank"
-
     end
 
   end
