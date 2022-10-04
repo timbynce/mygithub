2d24220811f26a94ddb3f45e100636fe708de309
timbynce <cherar90@gmail.com>
2022-10-04 13:53:21 +0300

    done screencast

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -12,7 +12,17 @@ class AnswersController < ApplicationController
   end
 
   def create
-    @answer = @question.answers.create(answer_params)
+    @answer = @question.answers.new(answer_params)
+
+    respond_to do |format|
+      if @answer.save
+        format.json { render json: @answer }
+      else
+        format.json do 
+          render json: @answer.errors.full_messages, status: :unprocessable_entity
+        end
+      end
+    end
   end
 
   def update
